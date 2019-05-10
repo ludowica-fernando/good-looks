7d7929b0c9f403c794c07646e2512eb176690ffb
cavindi <Beavermusic97!>
2019-05-10 18:56:29 +0530

    added filter products

---
+++ src/app/models/category.ts
@@ -0,0 +1,13 @@
+export class Category {
+    public id: number;
+    public image: string;
+    public keyword: string;
+    public name: string;
+
+    constructor() {
+        this.id = null;
+        this.image = null;
+        this.keyword = null;
+        this.name = null;
+    }
+}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,6 +2,7 @@ import { SessionStorageService } from './../services/session-storage.service';
 import { CartService } from './../services/cart.service';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -11,11 +12,23 @@ import { ProductService } from '../services/product.service';
 export class ProductsComponent implements OnInit {
 
   productList = [];
-  constructor(private productService: ProductService, private sessionService: SessionStorageService, private cartService: CartService) { }
+  filterList = [];
+  categorySelected;
+
+  constructor(
+    private productService: ProductService,
+    private sessionService: SessionStorageService,
+    private cartService: CartService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+
+    this.categoryFilter();
+
     this.productService.getAll().subscribe(results => {
       this.productList = results;
+
     });
   }
 
@@ -28,6 +41,18 @@ export class ProductsComponent implements OnInit {
     };
 
     this.cartService.addOrUpdate(cartItem).subscribe();
+  }
+
+  categoryFilter() {
+    this.route.queryParamMap.subscribe(params => {
+      // this.categorySelected = parseInt(params.get('category'));
+      this.categorySelected = params.get('category');
+      console.log(this.categorySelected)
+
+      this.filterList = (this.categorySelected) ?
+        this.productList.filter(product => product.category === this.categorySelected) :
+        this.productList;
+    });
   }
 
 }
